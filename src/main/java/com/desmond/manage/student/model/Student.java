79330baf2044e09b1f9b55df87ce0a9c4256ac6c
Desmond419 <7547461@gmail.com>
2021-09-13 16:17:49 +0800

    update model

--- src/main/java/com/desmond/manage/student/model/Student.java
+++ src/main/java/com/desmond/manage/student/model/Student.java
@@ -1,6 +1,70 @@
 package com.desmond.manage.student.model;
 
+import java.util.Date;
+
 public class Student {
-	private long id;
+	private int id;
 	private String fullname;
+	private int age;
+	private long icNo;
+	private int mobileNo;
+	private String emailAddress;
+	private String course;
+	private Date createDate;
+	private Date updateDate;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFullname() {
+		return fullname;
+	}
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public long getIcNo() {
+		return icNo;
+	}
+	public void setIcNo(long icNo) {
+		this.icNo = icNo;
+	}
+	public int getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(int mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
 }
